7a0932409e7790ab9c1de92eba5b5d7013ecbd3d
ci <ci@localhost>
2026-02-24 01:41:35 +0000

    chore: update motia-cli to v0.1.0
    Automated update from release workflow.
    
    Changes:
    - Version: v0.1.0
    - x86_64 SHA256: 024c2ce8e85c29259bee03e2fe9492329b40fcc3135584577f6182f7cf00f901
    - aarch64 SHA256: c4016c254812ac278b3a3e813761f319d1b57ab334a8b84633f625f525c43949

--- Formula/motia-cli.rb
+++ Formula/motia-cli.rb
@@ -1,14 +1,16 @@
 class MotiaCli < Formula
   desc "CLI for scaffolding Motia projects"
   homepage "https://github.com/MotiaDev/motia-cli"
+  license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/MotiaDev/motia-cli/releases/download/v0.1.0/motia-cli-aarch64-apple-darwin.tar.gz"
-      sha256 "placeholder"
-    else
+      sha256 "c4016c254812ac278b3a3e813761f319d1b57ab334a8b84633f625f525c43949"
+    end
+    on_intel do
       url "https://github.com/MotiaDev/motia-cli/releases/download/v0.1.0/motia-cli-x86_64-apple-darwin.tar.gz"
-      sha256 "placeholder"
+      sha256 "024c2ce8e85c29259bee03e2fe9492329b40fcc3135584577f6182f7cf00f901"
     end
   end
 
